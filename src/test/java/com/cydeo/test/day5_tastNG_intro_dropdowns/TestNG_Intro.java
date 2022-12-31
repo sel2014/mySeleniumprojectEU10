09bfbf0b68ac451578801e1d849171de66035b1e
Selcuk Cellik <sel2014@hotmail.com>
2022-12-31 19:28:11 +0000

    day5 TestNG_Intro

---
+++ src/test/java/com/cydeo/test/day5_tastNG_intro_dropdowns/TestNG_Intro.java
@@ -0,0 +1,14 @@
+package com.cydeo.test.day5_tastNG_intro_dropdowns;
+
+import org.testng.annotations.Test;
+
+public class TestNG_Intro {
+
+
+    @Test
+    public void test1(){
+
+        System.out.println("Test 1 is running..");
+
+    }
+}
