87e02b6e6e25f7cb0500aa9461ea45a9d014fc8a
Selcuk Cellik <sel2014@hotmail.com>
2023-01-03 14:09:06 +0000

    day6 Task6 state dropdown

--- src/test/java/com/cydeo/test/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/test/day6_alerts_iframes_windows/DropdownPractices.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -13,19 +14,17 @@ import java.util.concurrent.TimeUnit;
 public class DropdownPractices {
 
     public WebDriver driver;
+
     @BeforeMethod
     public void setupMethod() {
 
         //2. Go to http://practice.cydeo.com/dropdown
-
         driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://practice.cydeo.com/dropdown");
 
-
     }
-
 
     @Test
     public void dropdown_task5() throws InterruptedException {
@@ -34,11 +33,6 @@ public class DropdownPractices {
         //TC #5: Selecting state from State dropdown and verifying result
         //1. Open Chrome browser
         //2. Go to http://practice.cydeo.com/dropdown
-
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://practice.cydeo.com/dropdown");
 
         //we located the dropdown and it is ready to use
         Select stateDropdown = new Select(driver.findElement(By.xpath(
@@ -71,7 +65,44 @@ public class DropdownPractices {
     @Test
     public void dropdown_task6() {
 
+        //TC #6: Selecting date on dropdown and verifying
+        //1. Open Chrome browser
+        //2. Go to https://practice.cydeo.com/dropdown
 
+        //3. Select “December 1st, 1923” and verify it is selected.
+        Select yearDropdown = new Select(driver.findElement(By.xpath("//select[@id='year']")));
+        Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@id='month']")));
+        Select dayDropdown = new Select(driver.findElement(By.xpath("//select[@id='day']")));
+
+        //Select year using  : visible text
+        yearDropdown.selectByVisibleText("1924");
+
+        //Select month using   : value attribute
+        monthDropdown.selectByValue("11");
+
+        //Select day using : index number
+        dayDropdown.selectByIndex(0);
+
+        //creating expected values
+        String expectedYear = "1924";
+        String expectedMonth = "December";
+        String expecteDay = "1";
+
+        //getting actual values from browser
+        String actualYear = yearDropdown.getFirstSelectedOption().getText();
+        String actualMonth = monthDropdown.getFirstSelectedOption().getText();
+        String actualDay = dayDropdown.getFirstSelectedOption().getText();
+
+        //create assertions
+        Assert.assertTrue(actualYear.equals(expectedYear) );
+        Assert.assertEquals(actualMonth, expectedMonth);
+        Assert.assertEquals(actualDay, expecteDay);
+
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.close();
     }
 
 
