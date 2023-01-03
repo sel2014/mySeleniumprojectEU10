b69f1c070ec11c6a7007af8f787ee8893692ebd5
Selcuk Cellik <sel2014@hotmail.com>
2023-01-03 18:00:40 +0000

    day6 T5 window task

---
+++ src/test/java/com/cydeo/test/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -0,0 +1,52 @@
+package com.cydeo.test.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T5_Windows_Practice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //2. Go to website: https://practice.cydeo.com/windows
+        driver.get("https://practice.cydeo.com/windows");
+
+    }
+
+    @Test
+    public void multiple_window_test(){
+
+
+
+
+
+    }
+
+
+
+
+
+}
+
+/*
+TC #5: Window Handle practice
+
+1. Create a new class called: T5_WindowsPractice
+2. Create new test and make set ups
+3. Go to : https://practice.cydeo.com/windows
+4. Assert: Title is “Windows”
+5. Click to: “Click Here” link
+6. Switch to new Window.
+7. Assert: Title is “New Window”
+ */
