9d98a7099d095eb791284add06cea0979acf14ef
Selcuk Cellik <sel2014@hotmail.com>
2023-01-03 18:32:45 +0000

    day6 T5 window task

--- src/test/java/com/cydeo/test/day6_alerts_iframes_windows/T5_Windows_Practice.java
+++ src/test/java/com/cydeo/test/day6_alerts_iframes_windows/T5_Windows_Practice.java
@@ -2,6 +2,7 @@ package com.cydeo.test.day6_alerts_iframes_windows;
 
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -26,7 +27,21 @@ public class T5_Windows_Practice {
 
     @Test
     public void multiple_window_test(){
+       //storing the main page's window
+      String mainHandle = driver.getWindowHandle();
 
+        System.out.println("mainHandle = " + mainHandle);
+
+        //4. Assert: Title is “Windows”
+
+        String expectedTitle = "Windows";
+        String actualTitle = driver.getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitle);
+
+        //5. Click to: “Click Here” link
+        //6. Switch to new Window.
+        //7. Assert: Title is “New Window”
 
 
 
