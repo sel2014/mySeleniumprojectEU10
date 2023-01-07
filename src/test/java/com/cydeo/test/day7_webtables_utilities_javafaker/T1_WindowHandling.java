b3192320cdcb5d59136f931d322f389016f9bdc7
Selcuk Cellik <sel2014@hotmail.com>
2023-01-07 10:50:36 +0000

    day7 T1 window handling task

--- src/test/java/com/cydeo/test/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/test/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -1,8 +1,11 @@
 package com.cydeo.test.day7_webtables_utilities_javafaker;
 
+import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class T1_WindowHandling {
 
@@ -10,6 +13,9 @@ public class T1_WindowHandling {
 
     @BeforeMethod
     public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
 
