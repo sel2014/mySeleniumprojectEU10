f198136c235c302e1076d8471cd063b0ed8bca80
Selcuk Cellik <sel2014@hotmail.com>
2023-01-09 20:02:48 +0000

    day8 T4 Config_Practice

---
+++ src/test/java/com/cydeo/test/day8_properties_config_reader/T4_Config_Practice.java
@@ -0,0 +1,31 @@
+package com.cydeo.test.day8_properties_config_reader;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T4_Config_Practice {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+    }
+
+
+    @Test
+    public void google_search_test(){
+
+
+
+    }
+
+
+}
