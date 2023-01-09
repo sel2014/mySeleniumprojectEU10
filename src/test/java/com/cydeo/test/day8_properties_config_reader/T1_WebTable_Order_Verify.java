0524d0eadd1773782ba39815ac60e6f4c8775a36
Selcuk Cellik <sel2014@hotmail.com>
2023-01-09 15:36:04 +0000

    day8 T1_WebTable_Order_Verify

--- src/test/java/com/cydeo/test/day8_properties_config_reader/T1_WebTable_Order_Verify.java
+++ src/test/java/com/cydeo/test/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -1,6 +1,7 @@
 package com.cydeo.test.day8_properties_config_reader;
 
 import com.cydeo.utilities.WebDriverFactory;
+import com.cydeo.utilities.WebTablesUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -43,4 +44,24 @@ public class T1_WebTable_Order_Verify {
         Assert.assertEquals(bobMartinOrderDate.getText(), "12/31/2021");
 
     }
+
+    //We now use utility method we created for the task above
+    @Test
+    public void test2(){
+
+    String customerOrderDate1 = WebTablesUtils.returnOrderDate(driver, "Alexandra Gray");
+        System.out.println("customerOrderDate1 = " + customerOrderDate1);
+
+    }
+
+    //Using WebTableUtils.orderVerify(); method
+    @Test
+    public void test3(){
+
+                WebTablesUtils.orderVerify(driver, "Samuel Jackson",
+                        "12/21/2021");
+
+    }
+
+
 }
