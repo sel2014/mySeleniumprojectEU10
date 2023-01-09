3daf8728eb69d145b2dfb4fe9962aa52c3edbf95
Selcuk Cellik <sel2014@hotmail.com>
2023-01-09 18:51:11 +0000

    day8 ReadingProperties

--- src/test/java/com/cydeo/test/day8_properties_config_reader/LearningProperties.java
+++ src/test/java/com/cydeo/test/day8_properties_config_reader/LearningProperties.java
@@ -9,7 +9,11 @@ public class LearningProperties {
 
     public void java_properties_reading_test(){
 
+        //key = value
+        //os.name = Windows 11
+        //user.name = Sel20
         System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
 
     }
 
