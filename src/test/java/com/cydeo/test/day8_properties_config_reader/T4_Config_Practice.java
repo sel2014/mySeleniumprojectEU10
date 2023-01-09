df7264f7eade263ac7b2b74d0ff1833eeaf3ffeb
Selcuk Cellik <sel2014@hotmail.com>
2023-01-09 20:51:50 +0000

    day8 T4 Config_Practice

--- src/test/java/com/cydeo/test/day8_properties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/test/day8_properties_config_reader/T4_Config_Practice.java
@@ -1,7 +1,11 @@
 package com.cydeo.test.day8_properties_config_reader;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,6 +26,16 @@ public class T4_Config_Practice {
 
     @Test
     public void google_search_test(){
+
+        //3- Write “apple” in search box
+        WebElement googleSearchBox = driver.findElement(By.xpath("//input[@name='q']"));
+        WebElement acceptCookiesBox = driver.findElement(By.xpath("//*[@id=\"L2AGLb\"]/div"));
+        acceptCookiesBox.click();
+        googleSearchBox.sendKeys("apple"+ Keys.ENTER);
+
+        //4- Verify title:
+        Assert.assertEquals(driver.getTitle(), "apple - Google Search");
+        //Expected: apple - Google Search
 
 
 
