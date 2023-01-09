5001716cf5f952245591f1cc55546bc6d8cad84f
Selcuk Cellik <sel2014@hotmail.com>
2023-01-09 13:17:36 +0000

    day7 T3_CRM_LOGIN

--- src/test/java/com/cydeo/test/day8_properties_config_reader/T1_WebTable_Order_Verify.java
+++ src/test/java/com/cydeo/test/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -1,4 +1,18 @@
 package com.cydeo.test.day8_properties_config_reader;
 
+import org.testng.annotations.Test;
+
 public class T1_WebTable_Order_Verify {
+
+
+    @Test
+    public void order_name_verify_test(){
+
+        //1. Go to: https://practice.cydeo.com/web-tables
+        //2. Verify Bob’s name is listed as expected.
+        //Expected: “Bob Martin”
+        //3. Verify Bob Martin’s order date is as expected
+        //Expected: 12/31/2021
+
+    }
 }
