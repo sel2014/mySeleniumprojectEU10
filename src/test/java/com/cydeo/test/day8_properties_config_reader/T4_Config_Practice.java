5b10e340ef7a776f84ce972fae91e55fb6a872a1
Selcuk Cellik <sel2014@hotmail.com>
2023-01-14 14:11:37 +0000

    day9 JavaFakerPractice.java

--- src/test/java/com/cydeo/test/day8_properties_config_reader/T4_Config_Practice.java
+++ src/test/java/com/cydeo/test/day8_properties_config_reader/T4_Config_Practice.java
@@ -14,35 +14,45 @@ import java.util.concurrent.TimeUnit;
 
 public class T4_Config_Practice {
 
-    WebDriver driver;
+
+
+    public WebDriver driver;
 
     @BeforeMethod
     public void setupMethod(){
+
+        //We are getting the browserType dynamically from our configuration.properties file String browserType
+
         String browserType = ConfigurationReader.getProperty("browser");
-        driver= WebDriverFactory.getDriver(browserType);
+        driver = WebDriverFactory.getDriver(browserType);
 
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://google.com");
+        driver.get("https://www.google.com");
+
+         WebElement acceptCookiesBox = driver.findElement(By.xpath("//*[@id=\"L2AGLb\"]/div"));
+         acceptCookiesBox.click();
+
     }
 
-
-    @Test
-    public void google_search_test(){
+        @Test
+        public void google_search_test(){
 
         //3- Write “apple” in search box
-        WebElement googleSearchBox = driver.findElement(By.xpath("//input[@name='q']"));
-        WebElement acceptCookiesBox = driver.findElement(By.xpath("//*[@id=\"L2AGLb\"]/div"));
-        acceptCookiesBox.click();
-        googleSearchBox.sendKeys("apple"+ Keys.ENTER);
+            WebElement googleSearchBox = driver.findElement(By.xpath("//input[@name='q']"));
 
-        //4- Verify title:
-        Assert.assertEquals(driver.getTitle(), "apple - Google Search");
-        //Expected: apple - Google Search
+            googleSearchBox.sendKeys(ConfigurationReader.getProperty("searchValue") + Keys.ENTER);
 
+            //4- Verify title:
+            //Expected: apple - Google Search
+            String expectedTitle = ConfigurationReader.getProperty("searchValue")+" - Google Search";
+            String actualTitle = driver.getTitle();
 
+            Assert.assertEquals(actualTitle, expectedTitle);
+        }
 
     }
 
 
-}
+
+
---
+++ src/test/java/com/cydeo/test/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -0,0 +1,11 @@
+package com.cydeo.test.day9_javafaker_testbase_driverUtil;
+
+import org.testng.annotations.Test;
+
+public class JavaFakerPractice {
+
+    @Test
+    public void test1(){
+
+    }
+}
