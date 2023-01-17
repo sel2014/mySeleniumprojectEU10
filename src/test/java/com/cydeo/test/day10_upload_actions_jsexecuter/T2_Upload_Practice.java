6acd46f9a86776ab6a5943750e1e21ee07604db5
Selcuk Cellik <sel2014@hotmail.com>
2023-01-17 10:12:12 +0000

    day10 T1_Upload_Practice.java

---
+++ src/test/java/com/cydeo/test/day10_upload_actions_jsexecuter/T2_Upload_Practice.java
@@ -0,0 +1,28 @@
+package com.cydeo.test.day10_upload_actions_jsexecuter;
+
+import com.cydeo.utilities.Driver;
+import org.testng.annotations.Test;
+
+public class T2_Upload_Practice {
+
+    @Test
+    public void upload_test(){
+
+        Driver.getDriver().get("https://practice.cydeo.com/upload");
+
+
+    }
+
+
+}
+
+
+
+/*
+TC #2 Upload Test
+1. Go to https://practice.cydeo.com/upload
+2. Find some small file from your computer, and get the path of it.
+3. Upload the file.
+4. Assert:
+-File uploaded text is displayed on the page
+ */
