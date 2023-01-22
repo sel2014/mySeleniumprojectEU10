c0b6ef12e21068b6c2d14eb55173a82db5794bbf
Selcuk Cellik <sel2014@hotmail.com>
2023-01-22 11:11:30 +0000

    day11 T1_Circle_Drag_N_Drop.java

--- src/test/java/com/cydeo/test/day11_actions_jsexecuter_practice/T1_Circle_Drag_N_Drop.java
+++ src/test/java/com/cydeo/test/day11_actions_jsexecuter_practice/T1_Circle_Drag_N_Drop.java
@@ -1,8 +1,11 @@
 package com.cydeo.test.day11_actions_jsexecuter_practice;
 
+import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class T1_Circle_Drag_N_Drop {
@@ -19,8 +22,30 @@ public class T1_Circle_Drag_N_Drop {
         acceptAndCloseCookiesButton.click();
         //---------------------------------------------
 
-        WebElement bigCircle = Driver.getDriver().findElement(By.xpath("//div[@id='droptarget']"));
         WebElement smallCircle = Driver.getDriver().findElement(By.xpath("//div[@id='draggable']"));
+        WebElement bigCircle = Driver.getDriver().findElement(By.id("droptarget"));
+
+        Actions actions = new Actions(Driver.getDriver());
+        BrowserUtils.sleep(1);
+       // actions.dragAndDrop(smallCircle,bigCircle).perform();
+        actions.clickAndHold(smallCircle)
+                .pause(2000)
+                .moveToElement(bigCircle)
+                .pause(2000)
+                .release()
+                .perform(); //second solution with wait, step by step
+
+
+
+        String actualBigCircleText = bigCircle.getText();
+        String expectedBigCircleText = "You did great!";
+
+        Assert.assertEquals(actualBigCircleText,expectedBigCircleText);
+
+
+
+
+
 
 
 
